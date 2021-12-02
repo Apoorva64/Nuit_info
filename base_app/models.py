e5e0cb69a2c1c167fd5de9669ec6df3d62368674
Ekter Moi <ektermoi1@gmail.com>
2021-12-02 20:31:21 +0100

    added null to some data

--- base_app/models.py
+++ base_app/models.py
@@ -16,7 +16,7 @@ class RescueBoat(models.Model):
     has_been_published = models.BooleanField(verbose_name=_("has been published"))
     name = models.CharField(max_length=100, verbose_name=_("name"))
     id_plate = models.CharField(max_length=100, verbose_name=_('id plate'), null=True)
-    start_date = models.DateTimeField(verbose_name=_('start date'))
+    start_date = models.DateTimeField(verbose_name=_('start date'), null=True)
 
     def __str__(self):
         return self.name
@@ -25,7 +25,7 @@ class RescueBoat(models.Model):
 class MedalOfHonor(models.Model):
     has_been_published = models.BooleanField(verbose_name=_("has been published"))
     name = models.CharField(max_length=100, verbose_name=_("name"))
-    description = models.TextField(max_length=1000, verbose_name=_("description"))
+    description = models.TextField(max_length=1000, verbose_name=_("description"), null=True)
 
     def __str__(self):
         return self.name
@@ -35,9 +35,9 @@ class Rescuer(models.Model):
     has_been_published = models.BooleanField(verbose_name=_("has been published"))
     first_name = models.CharField(max_length=100, verbose_name=_("first name"))
     last_name = models.CharField(max_length=100, verbose_name=_("last name"))
-    description = models.TextField(max_length=1000, verbose_name=_("description"))
+    description = models.TextField(max_length=1000, verbose_name=_("description"), null=True)
     medals = models.ManyToManyField(MedalOfHonor, verbose_name=_("medals of honor"), null=True)
-    birth_date = models.DateTimeField(verbose_name=_("birth date"))
+    birth_date = models.DateTimeField(verbose_name=_("birth date"), null=True)
     death_date = models.DateTimeField(verbose_name=_("death date"), null=True)
 
     def __str__(self):
@@ -48,10 +48,10 @@ class Rescue(models.Model):
     has_been_published = models.BooleanField(verbose_name=_("has been published"))
     rescue_boats = models.ManyToManyField(RescueBoat, verbose_name=_("rescue boats"))
     rescued_boats = models.ManyToManyField(RescuedBoat, verbose_name=_("rescued boats"), null=True)
-    rescuers = models.ManyToManyField(Rescuer, verbose_name=_('rescuers'))
+    rescuers = models.ManyToManyField(Rescuer, verbose_name=_('rescuers'), null=True)
     rescue_date = models.DateTimeField(verbose_name=_("rescue date"))
-    number_of_saved_people = models.IntegerField(verbose_name=_("number of saved people"))
-    description = models.TextField(verbose_name=_("description"))
+    number_of_saved_people = models.IntegerField(verbose_name=_("number of saved people"), null=True)
+    description = models.TextField(verbose_name=_("description"), null=True)
 
     def __str__(self):
         return self.description
@@ -66,10 +66,11 @@ class User(AbstractUser):
 
 
 class RescueStation(models.Model):
+    has_been_published = models.BooleanField(verbose_name=_("has been published"))
     name = models.CharField(max_length=100, verbose_name=_("name"))
-    description = models.TextField(verbose_name=_("description"))
-    latitude = models.FloatField(verbose_name=_("latitude"))
-    longitude = models.FloatField(verbose_name=_("longitude"))
+    description = models.TextField(verbose_name=_("description"), null=True)
+    latitude = models.FloatField(verbose_name=_("latitude"), null=True)
+    longitude = models.FloatField(verbose_name=_("longitude"), null=True)
     rescuers = models.ManyToManyField(Rescuer, verbose_name=_("rescuers"), null=True)
 
     def __str__(self):
@@ -77,6 +78,7 @@ class RescueStation(models.Model):
 
 
 class Quote(models.Model):
+    has_been_published = models.BooleanField(verbose_name=_("has been published"))
     content = models.TextField(verbose_name=_("quote content"))
     rescue = models.ForeignKey(Rescue, verbose_name=_("rescue"), on_delete=models.SET_NULL, null=True)
     rescuer = models.ForeignKey(Rescuer, verbose_name=_("rescuer"), on_delete=models.SET_NULL, null=True)
@@ -92,9 +94,10 @@ ART_TYPE_CHOICES = (("book", _("book")),
 
 
 class Art(models.Model):
-    author = models.CharField(max_length=100, )
+    has_been_published = models.BooleanField(verbose_name=_("has been published"))
+    author = models.CharField(max_length=100)
     type = models.CharField(max_length=300, choices=ART_TYPE_CHOICES, verbose_name=_("type"))
-    creation_date = models.DateTimeField(verbose_name=_("creation date"))
+    creation_date = models.DateTimeField(verbose_name=_("creation date"), null=True)
     description = models.TextField(verbose_name=_("description"), null=True)
     editor_name = models.CharField(max_length=100, verbose_name=_("editor name"), null=True)
     name = models.CharField(max_length=100, verbose_name=_("name"))
