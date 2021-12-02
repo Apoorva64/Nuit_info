7bd0604e59192731c1fe867e58c73291101c0658
Apoorva64 <appadooapoorva@gmail.com>
2021-12-03 00:04:20 +0100

    compiled translations

--- base_app/urls.py
+++ base_app/urls.py
@@ -5,5 +5,21 @@ app_name = 'base_app'
 urlpatterns = [path('', views.index_view, name='index'),
                path('medal_of_honor_detail/<int:pk>/', views.MedalOfHonorDetailView.as_view(),
                     name="medal_of_honor_detail"),
-               path('medal_of_honor_list/', views.MedalOfHonorListView.as_view(), name="medal_of_honor_list")
+               path('medal_of_honor_list/', views.MedalOfHonorListView.as_view(), name="medal_of_honor_list"),
+
+               path('rescue_boat_detail/<int:pk>/', views.RescueBoatDetailView.as_view(),
+                    name="rescue_boat_detail"),
+               path('rescue_boat_list/', views.MedalOfHonorListView.as_view(), name="rescue_boat_list"),
+
+               path('rescue_station_detail/<int:pk>/', views.RescueStationDetailView.as_view(),
+                    name="rescue_station_detail"),
+               path('rescue_station_list/', views.RescueStationListView.as_view(), name="rescue_station_list"),
+
+               path('rescued_boat_detail/<int:pk>/', views.RescuedBoatDetailView.as_view(),
+                    name="rescued_boat_detail"),
+               path('rescued_boat_list/', views.RescuedBoatListView.as_view(), name="rescued_boat_list"),
+
+               path('rescuer_detail/<int:pk>/', views.RescuerDetailView.as_view(),
+                    name="rescuer_detail"),
+               path('rescuer_list/', views.RescuerListView.as_view(), name="rescuer_list"),
                ]
